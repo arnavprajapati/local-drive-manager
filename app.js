b9efbcfcaf8c47e26eeb27eaba436a6a2625b1a3
Arnav <arnavprajapati3101@gmail.com>
2025-07-17 22:51:33 +0530

    file visible using static

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import express from 'express'
+import { readdir } from 'fs/promises'
 
 const app = express()
+
 
 app.use('/', (req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*')
     next()
 })
 
-app.get('/', (req, res) => {
-    res.json(["test.txt", "image.png"])
+
+app.get('/', async (req, res) => {
+    const fileList = await readdir("./storage")
+    res.json(fileList)
 })
 
 app.listen(5700, () => {
     console.log("server started at port 5700");
 })
+app.use(express.static("storage"))
