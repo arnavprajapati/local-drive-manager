7ac5a7bca5a24ffbc82cca4f9c9f3a8467d1cef7
Arnav <arnavprajapati3101@gmail.com>
2025-07-17 23:45:35 +0530

    open and download using dynamic routing

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ app.use('/', (req, res, next) => {
 
 // app.use(express.static("storage"))
 
-// Serving Files 
-app.use((req, res, next) => {
-    console.log(req.query.action);
+// Serving Files using dynamic routing 
+app.get('/:filename', (req, res, next) => {
+    console.log(req.url);
+    const { filename } = req.params
+    console.log(filename);
     if(req.query.action === 'download'){
         res.set('Content-Disposition', 'attachment')
     }
-    express.static("storage")(req, res, next)
+    res.sendFile(`${import.meta.dirname}/storage/${filename}`)
 })
 
 // Serving Directory Content
